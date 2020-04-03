5f428d2c1e0ca643b4846b8960847bb4dc14c1df
Alan Halpin <C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.ie>
2020-04-03 22:42:35 +0200

    Functionality

--- mathGenApp/app.js
+++ mathGenApp/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,10 +18,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: "dont_reveal"}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mathGenApp/routes/index.js
+++ mathGenApp/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+function createMathObj(){
+  var newMathGame = {
+    title: "Addition",
+    numberList: []
+  };
+  for (var i = 0; i < 10; i++) {
+    newMathGame.numberList.push({
+      index: i,
+      randomNumber1: Math.round(Math.random() * 50),
+      randomNumber2: Math.round(Math.random() * 50)
+    });
+  }
+  return newMathGame;
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  newMathSheet = createMathObj();
+  res.render('index', newMathSheet);
 });
 
 module.exports = router;
