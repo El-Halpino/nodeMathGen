aeee9ab6f08d84363a59bd088fdc558b55f21bf9
Alan Halpin <C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.iegit config --global user.email C00229361@itcarlow.ie>
2020-04-04 19:13:44 +0200

    index fix

--- mathGenApp/routes/index.js
+++ mathGenApp/routes/index.js
@@ -18,7 +18,7 @@ function createMathObj(){
 }
 
 function checkAnswers(req){
-  console.log(req.query); // this is probably an answer, since we have data in the in session and the req.query
+  console.log(answers); // this is probably an answer, since we have data in the in session and the req.query
   var gameLength = mathGame.numberList.length;
   console.log(gameLength);
   var correctAnswerCount = 0;
@@ -27,7 +27,7 @@ function checkAnswers(req){
     console.log(JSON.stringify(mathGame.numberList[index]))
     var op1 = parseInt(question.randomNumber1, 10);
     var op2 = parseInt(question.randomNumber2, 10);
-    var answerToCheck = req.query["answer_" + index]; //Error here, 'answer_0' of undefined
+    var answerToCheck = answers["answer_" + index]; //Error here, 'answer_0' of undefined
     if (op1 + op2 == answerToCheck) {
       correctAnswerCount++;
     }
@@ -43,10 +43,11 @@ router.get('/', function(req, res, next) {
     req.session.hasOwnProperty("mathGame")
   )
   {
-    mathGame = req.session.mathGame
-    correctAns = checkAnswers(req.query, mathGame);
+    mathGame = req.session.mathGame;
+    answers = req.query;
+    correctAns = checkAnswers(answers, mathGame);
     delete req.session.mathGame;
-    res.render("result", correctAns)
+    res.render("result", correctAns);
   }
   else if (!req.session.hasOwnProperty("mathGame")) {
   newMathSheet = createMathObj();
